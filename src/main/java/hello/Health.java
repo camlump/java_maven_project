04415d0a1f93f56eea7f353c5f32daeae3a2a1b4
Cameron Lump <cameronlumpMacAir@Camerons-MacBook-Air.local>
2024-01-29 00:13:52 -0500

    added health string check

---
+++ src/main/java/hello/Health.java
@@ -0,0 +1,7 @@
+package hello;
+
+public class Health {
+    public String healthCheck() {
+        return "Healthy!";
+    }
+}
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,7 +7,9 @@ public class HelloWorld {
     LocalTime currentTime = new LocalTime();
     System.out.println("The current local time is: " + currentTime);
     Greeter greeter = new Greeter();
+    Health health = new Health();
     System.out.println(greeter.sayHello());
+    System.out.println(health.healthCheck());
     System.out.println("test build");
   }
 }
